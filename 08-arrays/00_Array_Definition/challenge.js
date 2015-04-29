79b917f18ab96b8754245600850e111c48204eb3
Stewart McCoy <mccoy.stewart@gmail.com>
2015-04-29 19:16:51 +0000

    complete challenge for arrays checkpoint

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,14 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function (arg1, arg2, arg3, arg4) {
+    return [arg1, arg2, arg3, arg4];
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(arg) {
+    var lastElementIndex = arg.length -1;
+    if ((typeof arg[0] === 'string') &&
+        (typeof arg[lastElementIndex] === 'string')) {
+            return [arg[0].toString(), arg[lastElementIndex].toString()];
+    } else if ((typeof arg[0] === 'number') &&
+        (typeof arg[lastElementIndex] === 'number')) {
+            return [arg[0], arg[lastElementIndex]];
+    }
+};
