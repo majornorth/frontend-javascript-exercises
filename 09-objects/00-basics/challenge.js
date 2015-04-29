c8a3d3904333153e4753dcdce817f7a9c1580973
Stewart McCoy <mccoy.stewart@gmail.com>
2015-04-29 21:09:59 +0000

    complete challenge for objects checkpoint

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,30 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+    return {
+        title: courseTitle,
+        duration: courseDuration,
+        students: courseStudents
+    };
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+    if (!(object.hasOwnProperty(newProp))) {
+        object[newProp] = newValue;
+    }
+    return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+    return ("Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender);
+};
 
-module.exports.canIGet = undefined;
+
+module.exports.canIGet = function(item, money) {
+    var priceTable = {
+        'MacBook Air': 999,
+        'MacBook Pro': 1299,
+        'Mac Pro': 2499,
+        'Apple Sticker': 1
+    }
+
+    return money >= priceTable[item];
+};
