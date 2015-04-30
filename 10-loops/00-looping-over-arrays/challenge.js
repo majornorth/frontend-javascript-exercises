6bc38b7b953f0ad63da392154fe4f8016db37423
Stewart McCoy <mccoy.stewart@gmail.com>
2015-04-30 01:12:58 +0000

    complete challenge for looping over arrays

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,44 @@
-module.exports.sumNumbers = undefined;
+// Why doesn't first function work?
 
-module.exports.splitAndLowerCaseString = undefined;
+// module.exports.sumNumbers = function(arr) {
+//     for (var counter = 0; counter < arr.length; counter++) {
+//         arr = arr[counter] + arr[(counter+1)];
+//     }
+//     return arr;
+// };
 
-module.exports.addIndex = undefined;
+// Found the answer below by searching 'function sum javascript array'
+// and reading StackOverflow
+
+module.exports.sumNumbers = function(arr) {
+    var count = 0;
+    for(var i = 0; i < arr.length; i++) {
+        count = count + arr[i];
+    }
+    return count;
+};
+
+module.exports.splitAndLowerCaseString = function(inputString) {
+    // var res = inputString.toLowerCase().split(" ");
+    // console.log(res);
+    // return res;
+
+    var findComma = ",";
+    var commaExists = inputString.indexOf(findComma);
+    if (!(commaExists === -1)) {
+        var res = inputString.toLowerCase().split(",");
+        return res;
+    } else {
+        var res = inputString.toLowerCase().split();
+        return res;
+    }
+
+};
+
+module.exports.addIndex = function(arr) {
+    var newArr = [];
+    for(var i = 0; i < arr.length; i++) {
+        newArr.push(i + " is " + arr[i]);
+    }
+    return newArr;
+};
